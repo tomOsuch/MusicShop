1446785153938cb2877a0251d6abb8a7ebf413c7
tomOsuch <tomasz.osuch@gmail.com>
2020-05-17 01:30:41 +0200

    Dodanie paska nawigacyjnego MVC SiteMap

---
+++ MusicShop/Infrastructure/ProductDetailsDynamicNodeProvider.cs
@@ -0,0 +1,33 @@
+﻿using MusicShop.DAL;
+using MusicShop.Models;
+using MvcSiteMapProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicShop.Infrastructure
+{
+    public class ProductDetailsDynamicNodeProvider:DynamicNodeProviderBase
+    {
+
+        private StoreContext db = new StoreContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var returneValue = new List<DynamicNode>();
+
+            foreach(Album album in db.Albums)
+            {
+                DynamicNode dynamicNode = new DynamicNode();
+                dynamicNode.Title = album.AlbumTitle;
+                dynamicNode.Key = "Album_" + album.AlbumId;
+                dynamicNode.ParentKey = "Genre_" + album.GenreId;
+                dynamicNode.RouteValues.Add("id", album.AlbumId);
+                returneValue.Add(dynamicNode);
+            }
+
+            return returneValue;
+        }
+    }
+}
---
+++ MusicShop/Infrastructure/ProduktListDynamicNodeProvider.cs
@@ -0,0 +1,32 @@
+﻿using MusicShop.DAL;
+using MusicShop.Models;
+using MvcSiteMapProvider;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Web;
+
+namespace MusicShop.Infrastructure
+{
+    public class ProduktListDynamicNodeProvider:DynamicNodeProviderBase
+    {
+
+        private StoreContext db = new StoreContext();
+
+        public override IEnumerable<DynamicNode> GetDynamicNodeCollection(ISiteMapNode node)
+        {
+            var returnValue = new List<DynamicNode>();
+            
+            foreach(Genre genre in db.Genres)
+            {
+                DynamicNode dynamicNode = new DynamicNode();
+                dynamicNode.Title = genre.Name;
+                dynamicNode.Key = "Genre_" + genre.GenreId;
+                dynamicNode.RouteValues.Add("genrename", genre.Name);
+                returnValue.Add(dynamicNode);
+            }
+
+            return returnValue;
+        }
+    }
+}
